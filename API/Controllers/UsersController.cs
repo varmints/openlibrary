f45eb08489528206b3e9130a2467becedeb0f33d
Kamil <you@example.com>
2021-09-24 13:26:56 +0200

    add client in angular

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
@@ -28,6 +30,5 @@ namespace API.Controllers
         {
             return await _context.Users.FindAsync(id);
         }
-
     }
 }
